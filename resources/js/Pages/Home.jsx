3a525550604365181aa07af49c30013f5d236ec7
jaynevernice <jaynevernice.professional@gmail.com>
2024-02-10 08:23:24 +0800

    Swiper working but not working as expected for grid

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -4,31 +4,13 @@ import {FaArrowDown, FaArrowUp, FaFont, FaSwatchbook, FaBook} from "react-icons/
 import { IconContext } from 'react-icons'
 import { cards } from '@/Data'
 
+import {Swiper, SwiperSlide} from 'swiper/react'
+import 'swiper/css'
+
 function Home() {
-  // const [rotateValue, setRotateValue] = useState("rotate(0deg)");
-  
-  // useEffect(() => {
-  //   const circle = document.getElementById("circle");
-  //   const upBtn = document.getElementById("upBtn");
-  //   const downBtn = document.getElementById("downBtn");
-
-  //   const rotateHandler = (angle) => {
-  //     const newRotateValue = 'rotate(${angle}deg)';
-  //     setRotateValue(newRotateValue);
-  //     circle.style.transform = newRotateValue;
-  //   };
-
-  //   upBtn.onclick = () => rotateHandler(-90);
-  //   downBtn.onclick = () => rotateHandler(90);
-
-  //   return () => {
-  //     upBtn.onclick = null;
-  //     downBtn.onclick = null;
-  //   };
-  // }, []);
 
   const [categoryIndex, setCategoryIndex] = useState(0);
-  // const categories = ['Typography', 'Color', 'Icons', 'Documentation'];
+
   const categories = [
     {name: 'Typography', icon: <FaFont/>},
     {name: 'Color', icon: <FaSwatchbook/>},
@@ -69,14 +51,21 @@ function Home() {
             </div>
           </div>
 
-          {/* <div className="circle"> */}
 
-          <div className='absolute top-1/2 left-full transform -translate-x-1/2 -translate-y-1/2'>
-            <div className='grid grid-cols-2 gap-x-[21rem] gap-y-2'>
+          <Swiper className='absolute top-1/2 left-full transform -translate-x-1/2 -translate-y-1/2'
+            spaceBetween={10}
+            slidesPerView={3}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+          {/* <div className='absolute top-1/2 left-full transform -translate-x-1/2 -translate-y-1/2'> */}
+            {/* <div className='grid grid-cols-2 gap-x-[21rem] gap-y-2'> */}
               {filteredCards.map(({name, site, logo, description}, index) => {
+              // {filteredCards.slice(0, 8).map(({name, site, logo, description}, index) => {
                 // console.log({name, site, logo, description});
                 return(
-                  <div key={index}>
+                  <SwiperSlide key={index} >
+                    {/* div key={index} */}
                     <a href={site} target='_blank'>
                       <div className="text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-80 h-40">
                         <div className="p-6">
@@ -90,115 +79,12 @@ function Home() {
                         </div>
                       </div>
                     </a>
-                  </div>
+                  </SwiperSlide>
                 )
               })}
-            </div>
-          </div>
-
-
-            {/* {typography.map(({name, site, logo, description}, index) => {
-
-              console.log({name, site, logo, description});
-
-              return(
-                <div className="" key={index}>
-                  <div key={index}>
-                    <a href={site} target='_blank'>
-                      <div className="absolute left-72 flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-10">
-                        <div className="p-6">
-                          <img src={logo} />
-                          <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-                            {name}
-                          </h5>
-                          <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-                            {description}
-                          </p>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                </div>
-              )
-            })} */}
-            
-            {/* <div className='absolute flex-row top-72 left-2/3 m-4'>
-              <div className="">
-                <a href="https://fonts.google.com/" target='_blank'>
-                <div className="text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
-                  <div className="p-6">
-                    <img src="https://logos-world.net/wp-content/uploads/2021/03/Google-Fonts-Logo.png" alt="" className='h-10' />
-                    <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-                      Google Fonts
-                    </h5>
-                    <p className=" font-sans text-base antialiased font-light leading-relaxed text-inherit">
-                      A computer font and web services owned by Google
-                    </p>
-                  </div>
-                </div>
-                </a>
-              </div>
-              <div className="">
-                <a href="https://fontsinuse.com/" target='_blank'>
-                <div className="text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
-                  <div className="p-6">
-                    <img src="https://seeklogo.com/images/F/fonts-in-use-logo-6EA99A0816-seeklogo.com.png" alt="" className='h-10' />
-                    <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-                      Fonts in Use
-                    </h5>
-                    <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-                      A computer font and web services owned by Google
-                    </p>
-                  </div>
-                </div>
-                </a>
-              </div>
-             </div> */}
-
-
-            {/* <div className="category two">
-              <a href=""></a>
-            </div> */}
-
+            {/* </div> */}
           {/* </div> */}
-
-
-        
-
-
-        {/* <div id='circle' style={{ transform: rotateValue }}>
-          <div className='feature one'>
-            <FaCamera />
-            <div>
-              <h1>Camera</h1>
-              <p>12MP, Wide Angle Lens</p>
-            </div>
-          </div>
-
-          <div className='feature two'>
-            <FaCalendar />
-            <div>
-              <h1>Processor</h1>
-              <p>Snapdragon Octa-core 11nm</p>
-            </div>
-          </div>  
-
-          <div className='feature three'>
-            <FaPhone />
-            <div>
-              <h1>Display</h1>
-              <p>0.5 Mini-Drop Fullscreen</p>
-            </div>
-          </div>  
-
-          <div className='feature four'>
-            <FaBatteryFull />
-            <div>
-              <h1>Battery Life</h1>
-              <p>5000mAh, 720Hrs Standby</p>
-            </div>
-          </div>  
-        </div> */}
+          </Swiper>
 
       </div>
     </div>
